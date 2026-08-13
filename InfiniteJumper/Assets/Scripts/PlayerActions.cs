d29f4f1d454974e0f666c44a8fceb72a51a78063
Lucas Galli <luquigalli0207gmail.com>
2026-08-13 12:34:35 -0300

    add player actions script

--- InfiniteJumper/Assets/Scripts/PlayerActions.cs
+++ InfiniteJumper/Assets/Scripts/PlayerActions.cs
@@ -2,13 +2,47 @@ using UnityEngine;
 
 public class PlayerActions : MonoBehaviour
 {
+    [SerializeField] private float jumpForce = 5f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
     void Start()
     {
-        float playerSpeed = 30.0f;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        // El manejo de Inputs en Update() es correcto para no perder pulsaciones
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            // Nota: En Unity 2025/2026 se usa rb.linearVelocity en lugar del antiguo rb.velocity. ¡Bien ahí!
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            // Evaluamos el primer punto de contacto
+            // Una normal.y cercana a 1 significa que el suelo está completamente plano abajo
+            if (collision.contacts[0].normal.y > 0.7f)
+            {
+                isGrounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = false;
+        }
     }
 }
