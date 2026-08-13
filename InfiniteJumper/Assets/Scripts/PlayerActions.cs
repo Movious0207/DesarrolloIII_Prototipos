99e3660aec1690522d7b23ecd4d611904a0f8c08
Lucas Galli <luquigalli0207gmail.com>
2026-08-13 11:35:54 -0300

    move Jump prototype to separate folder

---
+++ InfiniteJumper/Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    void Start()
+    {
+        float playerSpeed = 30.0f;
+    }
+
+    void Update()
+    {
+        
+    }
+}
